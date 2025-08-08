683204f22ca1f4e7055ebc88e200e9caad44697d
Anson <43693662+yeewai94@users.noreply.github.com>
2025-08-08 12:01:37 +0800

    Added an event listener to numbers using event delegation Stored and display user input on the display div with a maximum of 2-digits

--- javascript.js
+++ javascript.js
@@ -14,20 +14,46 @@ function divide(a,b){
     return a / b;
 }
 
-let number;
-let operator;
-let anotherNumbers;
-
-function operate(numbers, operators, otherNumbers){
-    if (operators === "+") return add(numbers, otherNumbers);
-    else if (operators === "-") return subtract(numbers, otherNumbers);
-    else if (operators === "*") return multiply(numbers, otherNumbers);
-    else if (operators === "/") return divide(numbers, otherNumbers);
+function operate(firstNumber, operator, secondNumber){
+    if (operator === "+") return add(firstNumber, secondNumber);
+    else if (operator === "-") return subtract(firstNumber, secondNumber);
+    else if (operator === "*") return multiply(firstNumber, secondNumber);
+    else if (operator === "/") return divide(firstNumber, secondNumber);
     else return
 }
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let firstDigit = "";
+let secondDigit = "";
+
 const display = document.querySelector(".display");
 const clear = document.querySelector(".clear").addEventListener("click", () => {
    return display.innerText = "";
 });
 
-// const btn = document.querySelectorAll("button").forEach()
+const numbers = document.querySelector(".numbers").addEventListener("click", (e) => {
+    if (!firstDigit){
+        firstDigit = e.target.innerText;
+    }
+    else if (firstDigit && !secondDigit){
+        secondDigit = e.target.innerText;
+    }
+    else {
+        return display.innerText = `${firstDigit}${secondDigit}`;
+    }
+    firstNumber = `${firstDigit}${secondDigit}`;  
+    return display.innerText = firstNumber;  
+
+});
+
+
+const operators = document.querySelector(".operators").addEventListener("click", (e) => {
+    operator = e.target.innerText;
+    if (operator !== "="){
+        return display.innerText = operator;
+    }
+});
+    
+
