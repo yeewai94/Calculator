c821fd16a1103cd8ee81d29c85a5598c76f6873e
Anson <43693662+yeewai94@users.noreply.github.com>
2025-08-08 15:36:58 +0800

    Tried to use array method to store the input value and return to string

--- javascript.js
+++ javascript.js
@@ -14,46 +14,63 @@ function divide(a,b){
     return a / b;
 }
 
-function operate(firstNumber, operator, secondNumber){
-    if (operator === "+") return add(firstNumber, secondNumber);
-    else if (operator === "-") return subtract(firstNumber, secondNumber);
-    else if (operator === "*") return multiply(firstNumber, secondNumber);
-    else if (operator === "/") return divide(firstNumber, secondNumber);
-    else return
+function operate(first, operator, second){
+    if (operator === "+") return add(first, second);
+    else if (operator === "-") return subtract(first, second);
+    else if (operator === "*") return multiply(first, second);
+    else if (operator === "/") return divide(first, second);
 }
 
-let firstNumber = "";
-let secondNumber = "";
-let operator = "";
-let firstDigit = "";
-let secondDigit = "";
+let array = [];
+let previousNumber = "";
+let currentNumber = "";
 
 const display = document.querySelector(".display");
 const clear = document.querySelector(".clear").addEventListener("click", () => {
    return display.innerText = "";
 });
 
-const numbers = document.querySelector(".numbers").addEventListener("click", (e) => {
-    if (!firstDigit){
-        firstDigit = e.target.innerText;
+const items = document.querySelector(".items").addEventListener("click", (e) => {
+    const item = e.target.class;
+    if(e.target.classList.contains("numbers")){
+        array.push(e.target.innerText);
+        currentNumber = array.join("");
+        display.innerText = currentNumber;
     }
-    else if (firstDigit && !secondDigit){
-        secondDigit = e.target.innerText;
-    }
-    else {
-        return display.innerText = `${firstDigit}${secondDigit}`;
-    }
-    firstNumber = `${firstDigit}${secondDigit}`;  
-    return display.innerText = firstNumber;  
-
-});
-
-
-const operators = document.querySelector(".operators").addEventListener("click", (e) => {
-    operator = e.target.innerText;
-    if (operator !== "="){
-        return display.innerText = operator;
+    else if (e.target.classList.contains("operator")){
+        array = [];
+        previousNumber = currentNumber;
+        let result = operate(Number(previousNumber), e.target.innerText, Number(currentNumber));
+        display.innerText = result;
+       
+        
     }
 });
+// const numbers = document.querySelector(".numbers").addEventListener("click", (e) => {
+//     if(!firstNumber){
+//         if (!firstDigit){
+//             firstDigit = e.target.innerText;
+//         }
+//         else if (firstDigit && !secondDigit){
+//             secondDigit = e.target.innerText;
+//         }
+//         firstNumber = `${firstDigit}${secondDigit}`;  
+//         display.innerText = firstNumber;  
+//     }
+//     else {
+//         if (!firstDigit){
+//             firstDigit = e.target.innerText;
+//         }
+//         else if (firstDigit && !secondDigit){
+//             secondDigit = e.target.innerText;
+//         }
+//         secondNumber = `${firstDigit}${secondDigit}`;  
+//         display.innerText = secondNumber;  
+//     }
+//     console.log(`${firstDigit}+${secondDigit}`);
+// });
+
+
+
     
 
